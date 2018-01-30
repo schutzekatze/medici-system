c21e2ba653389e582e0f48bf08bab4214c3641e8
schutzekatze <vladimir.nik94@gmail.com>
2018-01-30 02:56:53 +0100

    Change receipt dir

--- receipts/image_pooler/image_pooler.py
+++ receipts/image_pooler/image_pooler.py
@@ -1,12 +1,18 @@
 import logging
 import cv2
 import numpy
-import datetime
+import uuid
+import os
 
 logger = logging.getLogger(__name__)
 
 def pool_image(mediciuser, image):
-    path = '/tmp/' + mediciuser.user.username + ' ' + str(datetime.datetime.now()) + '.jpg'
+    image_dir = '/home/flux/receipts-unlabeled'
+
+    if len(os.listdir(image_dir)) > 2000:
+        return
+
+    path = image_dir + '/receipt-' + uuid.uuid4().hex + '.jpg'
 
     logger.info("Saving image from <" + mediciuser.user.username + "> to '" + path + "'.")
 
